cb8a90914e028c45d12d7205c8c324169d3721e5
Cody Thompson <codylthomp@gmail.com>
2023-09-28 21:15:08 -0500

    Javascript api Pulls

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
-console.log("hello")
+requestUrl1 = 'https://www.dnd5eapi.co/api/races'; //Races
+requestUrl = 'https://www.dnd5eapi.co/api/classes'; //classes
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+        var classes = data.results;
+        var randomIndex = Math.floor(Math.random() * classes.length); 
+        var chosenClass = classes[randomIndex];
+        console.log(chosenClass.name);
+
+       
+       
+       
+       
+  });
+
+
+
+
+
+  fetch(requestUrl1)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    var races = data.results;
+        var randomIndex = Math.floor(Math.random() * races.length); 
+        var chosenRace = races[randomIndex];
+        console.log(chosenRace.name);
+
+  });
