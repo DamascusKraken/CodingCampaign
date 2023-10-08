91b2ab00063910301a935f8fde00c591d2c1f79e
Cody Thompson <codylthomp@gmail.com>
2023-10-08 10:33:15 -0500

    updated progress bar

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
-requestUrl1 = 'https://www.dnd5eapi.co/api/races'; //Races
 requestUrl = 'https://www.dnd5eapi.co/api/classes'; //classes
+requestUrl1 = 'https://www.dnd5eapi.co/api/races'; //Races
+requestUrl2 = 'https://api.fungenerators.com/name/generate?category=dragon&limit=10'; //Names
+
+var charButton = $("#classGen");
+var raceButton = $("#raceGen");
+var statButton = $("#statGen");
+var nameButton = $("#nameGen");
+
 
 let degreesToRotate = 90;
 
@@ -8,11 +15,43 @@ var charButton = $("#classGen");
 var raceButton = $("#raceGen");
 var rerollButton = $("#rerollButton")
 var savedCharacter = $("#savedCharacter")
+var saveButton = $(".savebtn");
 var progressBar = {}; //evalutes if we have already updated the progress bar for this section.
 
+var strength; //creating varriables so we can use them outside of statgen scope.
+var wisdom;
+var constitution;
+var intelligence;
+var wisdom;
+var charisma;
+
+
 //Section for localStorage, need to change to an array later on.
-var characterClass = localStorage.getItem("characterClass");
-var classSave;
+
+var existingCharacters = localStorage.getItem('characters');
+var characters = JSON.parse(existingCharacters) || [];
+
+
+
+var characterClass = localStorage.getItem("characterClass"); //was using previously to setup localstorage.
+var characterRace = localStorage.getItem("characterRace");
+var characterName = localStorage.getItem("characterName");
+
+var newCharacter = { //object for our name race class and skills.
+  name: characterName,
+  race: characterRace,
+  class: characterClass,
+  stats: {
+    str: strength,
+    wis: wisdom,
+    con: constitution,
+    int: intelligence,
+    charis: charisma
+  }
+};
+
+
+
 
 
 charClass = function(event) { //class random creation function.
@@ -36,8 +75,8 @@ fetch(requestUrl)
           incrementProgressBar();
           progressBar[0] = true;
         }
-        classSave = chosenClass.name;
-        
+
+        newCharacter.class = chosenClass.name;
   
         
        
@@ -68,20 +107,160 @@ charRace = function(event) {
         progressBar[1] = true;
       }
 
+      
+      newCharacter.race = chosenRace.name;
+
   });
 }
 
+
+ statRollGen = function(event) { // Stat Roll Generator Function (4d6 drop lowest roll)
+
+  var statArray  = new Array(5);
+  for(var i = 0; i < 6; i++){
+      statArray[i] = statRoll();
+      console.log(statArray[i]);
+
+      //Setting values here so we can put in localstorage.
+      strength = statArray[0];
+      wisdom = statArray[1]; 
+      constitution = statArray[2];
+      intelligence = statArray[3];
+      charisma = statArray[4];
+
+      newCharacter.stats.str = strength; //setting values in our newCharacter object.
+      newCharacter.stats.wis = wisdom;
+      newCharacter.stats.con = constitution;
+      newCharacter.stats.int = intelligence;
+      newCharacter.stats.charis = charisma;
+
+      if (!progressBar[3]){ //We update the progress bar but only one time per section.
+        incrementProgressBar();
+        progressBar[3] = true;
+      }
+
+    console.log(newCharacter);
+      
+  }
+  //Renders the stat name and abilityScore
+  for(var i = 0; i < 6; i++){
+      var attributes = ["Strength: ", "Wisdom: ","Constitution: ", "Intelligence: ", "Wisdom: ", "Charisma: "];
+      statIndex = i +1;
+      var stat = $("#stat-" + statIndex);
+      stat.text(attributes[i] + statArray[i]);
+      
+  }
+}
+
+ 
+  statButton.on("click", statRollGen)
+
+function statRoll() {
+
+  var scores = new Array(3);
+  for(var i = 0; i < 4; i++){
+      var d6Roll  = (Math.floor((Math.random() * 6) + 1));
+      scores[i] = (d6Roll);
+      var rollNumber = i + 1;
+      console.log("Roll " + rollNumber + ": " + scores[i]);
+  }
+
+  var lowestRoll = getLowRoll(scores);
+  console.log("Lowest Roll Dropped: " + lowestRoll);
+
+  var abilityScore = 0;
+
+  for(var i = 0; i < 4; i++){
+      abilityScore += scores[i];
+  }
+
+  abilityScore -= lowestRoll;
+  
+  return abilityScore;
+};
+  
+function getLowRoll(scores){
+  var lowestRoll = 6;
+  var lowRoll;
+  for(var i = 0; i <= 3; i ++){
+      for(var j = 0; j <= 3; j++){
+          if(scores[i] < scores[j]){
+              lowRoll = scores[i];
+              if (lowRoll < lowestRoll){
+                  lowestRoll = lowRoll;
+              }
+          }
+      }
+  }
+
+  return lowestRoll;
+};
+
+
+charName = function(event) {
+
+  fetch(requestUrl2)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    var names = data.contents.names;
+    console.log(names);
+        var randomIndex = Math.floor(Math.random() * names.length);
+        var randomName = names[randomIndex];
+        console.log(randomName);
+      var buttonText = $("#nameText");
+      buttonText.text(randomName);
+      newCharacter.name = randomName;
+
+      if (!progressBar[4]){ //We update the progress bar but only one time per section.
+        incrementProgressBar();
+        progressBar[4] = true;
+      }
+});
+
+}
+
+nameButton.on("click", charName)
+
+saveFeature = function(){ //when we click save button. We save values to push into our character object.
+  console.log("saving!")
+  characters.push(newCharacter);
+
+  
+  var updatedCharacters = JSON.stringify(characters);
+  
+  
+  localStorage.setItem('characters', updatedCharacters);
+
+}
+
+
 savedCharacters = function(){ //beginging of saving characters. We need to add an array of things to append. Right now it will only append one item from our last. We also want to append a paragraph for each character.
-  localStorage.setItem("characterClass",classSave);
-  savedCharacter.append(characterClass)
   
 
+
+
+  characters.forEach(function(character) {
+    var savedCharacter = $("#savedCharacter")
+   
+    var paragraph = document.createElement('p');
+    
+    paragraph.textContent = character.name + ", The " + character.race + " " + character.class +  " Strength:" + character.stats.str +  " Wisdom:" + character.stats.wis +  " Constituion:" + character.stats.con +  " Intelligence:" + character.stats.int +  " Charisma:" + character.stats.charis;
+    paragraph.classList.add('is-size-6');
+
+   
+    savedCharacter.append(paragraph);
+  });
 }
 
 reroll = function(event){
   console.log("rerolling")
   charClass(event);
   charRace(event);
+  charName(event);
+  statRollGen(event);
   // Add your functions in here to run when we hit reroll.
 }
 
@@ -98,12 +277,15 @@ function incrementProgressBar() { //progress bar functionality, we call this aft
   $('progress').val(progressValue);
 }
 
-setInterval(diceRoll, 10);
+
 
 
   charButton.on("click", charClass)
   raceButton.on("click", charRace)
   rerollButton.on("click", reroll)
+  saveButton.on("click", saveFeature)
+  setInterval(diceRoll, 10);
   diceRoll();
-
   savedCharacters();
+
+
